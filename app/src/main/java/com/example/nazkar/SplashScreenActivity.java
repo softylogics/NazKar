a632ab34e318b3f7bf0ccad2aeb52f882ef52f39
Rehan Sid <softylogics@gmail.com>
2020-05-30 02:54:29 +0500

    New Screens

--- app/src/main/java/com/example/nazkar/SplashScreenActivity.java
+++ app/src/main/java/com/example/nazkar/SplashScreenActivity.java
@@ -1,10 +1,10 @@
 package com.example.nazkar;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
     @Override
